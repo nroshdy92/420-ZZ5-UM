76a177f8c81abba260cbd7171e3ad3f3ca47470a
nroshdy92 <156032373+nroshdy92@users.noreply.github.com>
2024-01-08 19:58:01 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,49 +1,15 @@
+variable "force_map" {​
 
-variable "subscription_id"{
-  type=string
-}
+  type = map(string)​
 
-variable "client_id"{
-  type=string
-}
-variable "client_secret"{
-  type=string
-}
-variable "tenant_id"{
-  type=string
-}
-variable "countNumber"{
-  type=number
-  default=0
-}
-variable "account_tier"{
-  type=string
-  default="Standard"
-}
-variable "myname"{
-  type=string
-}
-variable "account_replication_type"{
-  type=string
-  default="GRS"
-}
-variable "access_tier"{
-  type=string
-  default="Cool"
-}
-variable "cross_tenant_replication_enabled"{
- type=bool
- default=false
-}
-variable "prefix"{
- type=string
- default="mcit"
-}
-variable "component" {
-  type    = list
-  default = ["bastion", "frontproxy", "db", "infra"]
-}
-variable "environment"{
- type=string
- default="staging"
-}
+  default = {​
+
+    luke  = "jedi"​
+
+    yoda  = "jedi"​
+
+    darth = "sith"​
+
+  }​
+
+}​
