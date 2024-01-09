2e9db9ea6f5fd68edeb341bab447b7b59f1cf1de
nroshdy92 <156032373+nroshdy92@users.noreply.github.com>
2024-01-08 19:35:27 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -47,3 +47,89 @@ variable "environment"{
  type=string
  default="staging"
 }
+
+variable "example_map" {​
+
+  type = map(object({​
+
+    name = string​
+
+    enemies_destroyed = number​
+
+    badguy = bool​
+
+  }))​
+
+  default = {​
+
+    key1 = {​
+
+      name = "luke"​
+
+      enemies_destroyed = 4252​
+
+      badguy = false​
+
+    }​
+
+    key2 = {​
+
+      name = "yoda"​
+
+      enemies_destroyed = 900​
+
+      badguy = false​
+
+    }​
+
+    key3 = {​
+
+      name = "darth"​
+
+      enemies_destroyed=  20000056894​
+
+      badguy = true​
+
+    }​
+
+  }​
+
+}​
+
+variable "lightsabre_color_map" {​
+
+  type = map(list(string))​
+
+  default = {​
+
+    luke = ["green", "blue"]​
+
+    yoda = ["green"]​
+
+    darth = ["red"]​
+
+  }​
+
+}​
+
+This could also be achieved with a map using the ‘set’ type:​
+
+​
+
+variable "lightsabre_color_map" {​
+
+  type = map(set(string))​
+
+  default = {​
+
+    luke = ["green", "blue"]​
+
+    yoda = ["green"]​
+
+    darth = ["red"]​
+
+  }​
+
+}​
+
+​
